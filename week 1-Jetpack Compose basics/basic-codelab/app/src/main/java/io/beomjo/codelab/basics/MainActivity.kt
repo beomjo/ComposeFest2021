2993dc0fccd121c0aa3c70ea0d293b387a804c43
beomjo <qjatjr1108@gmail.com>
2021-11-05 03:39:24 +0900

    feat: Create onboarding and Greeting rows

--- week 1-Jetpack Compose basics/basic-codelab/app/src/main/java/io/beomjo/codelab/basics/MainActivity.kt
+++ week 1-Jetpack Compose basics/basic-codelab/app/src/main/java/io/beomjo/codelab/basics/MainActivity.kt
@@ -3,11 +3,14 @@ package io.beomjo.codelab.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.leanback.widget.Row
 import io.beomjo.codelab.basics.ui.theme.BasiccodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,23 +19,89 @@ class MainActivity : ComponentActivity() {
         setContent {
             BasiccodelabTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp() {
+    var shouldShowOnBoarding by remember { mutableStateOf(true) }
+
+    if (shouldShowOnBoarding) {
+        OnboardingScreen {
+            shouldShowOnBoarding = false
+        }
+    } else {
+        Greetings()
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+private fun Greetings(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(4.dp)) {
+        names.forEach { Greeting(name = it) }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    val isExpand = remember { mutableStateOf(false) }
+    val extraPadding = if (isExpand.value) 48.dp else 0.dp
+
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(extraPadding)
+            ) {
+                Text(text = "Hello")
+                Text(text = "$name!")
+            }
+            OutlinedButton(
+                onClick = {
+                    isExpand.value = !isExpand.value
+                }
+            ) {
+                Text(text = if (isExpand.value) "Show less" else "Show more")
+            }
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(onClickContinue: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basic Codelab!")
+            Button(
+                modifier = Modifier.padding(24.dp),
+                onClick = onClickContinue
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    name = "Text Preview",
+    widthDp = 320,
+    heightDp = 320
+)
 @Composable
 fun DefaultPreview() {
     BasiccodelabTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
