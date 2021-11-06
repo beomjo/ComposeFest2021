daef51e8d5a4786d2aad9a06b1760e40f36351dd
Beomjobs <bsjo@teamblind.com>
2021-11-07 01:23:15 +0900

    feat: Change to apply only padding bottom

--- week 1-Jetpack Compose basics/basic-codelab/app/src/main/java/io/beomjo/codelab/basics/MainActivity.kt
+++ week 1-Jetpack Compose basics/basic-codelab/app/src/main/java/io/beomjo/codelab/basics/MainActivity.kt
@@ -3,6 +3,9 @@ package io.beomjo.codelab.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -20,7 +23,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasiccodelabTheme {
-                // A surface container using the 'background' color from the theme
                 MyApp()
             }
         }
@@ -51,28 +53,34 @@ private fun Greetings(names: List<String> = List(1000) { "$it" }) {
 
 @Composable
 fun Greeting(name: String) {
-    val isExpand = remember { mutableStateOf(false) }
-    val extraPadding = if (isExpand.value) 48.dp else 0.dp
+    var isExpand by remember { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        targetValue = if (isExpand) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(extraPadding)
-            ) {
+        Row(
+            modifier = Modifier
+                .padding(24.dp)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp)),
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
                 Text(text = "Hello")
                 Text(text = "$name!")
             }
             OutlinedButton(
                 onClick = {
-                    isExpand.value = !isExpand.value
+                    isExpand = !isExpand
                 }
             ) {
-                Text(text = if (isExpand.value) "Show less" else "Show more")
+                Text(text = if (isExpand) "Show less" else "Show more")
             }
         }
     }
@@ -99,13 +107,28 @@ fun OnboardingScreen(onClickContinue: () -> Unit) {
 
 @Preview(
     showBackground = true,
-    name = "Text Preview",
+    name = "OnBoarding Preview",
     widthDp = 320,
     heightDp = 320
 )
 @Composable
-fun DefaultPreview() {
+fun OnboardingPreview() {
     BasiccodelabTheme {
-        MyApp()
+        OnboardingScreen {
+        }
     }
 }
+
+@Preview(
+    showBackground = true,
+    name = "Greetings Preview",
+    widthDp = 320,
+    heightDp = 320
+)
+@Composable
+fun GreetingsPreview() {
+    BasiccodelabTheme {
+        Greetings()
+    }
+}
+
