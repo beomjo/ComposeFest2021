6bbe53b08b8e479b99fc605a0ff6fd24112b9659
Beomjobs <bsjo@teamblind.com>
2021-11-07 01:44:15 +0900

    feat: Change AppStyle and Theme

--- week 1-Jetpack Compose basics/basic-codelab/app/src/main/java/io/beomjo/codelab/basics/MainActivity.kt
+++ week 1-Jetpack Compose basics/basic-codelab/app/src/main/java/io/beomjo/codelab/basics/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.beomjo.codelab.basics.ui.theme.BasiccodelabTheme
@@ -73,7 +74,12 @@ fun Greeting(name: String) {
         ) {
             Column(modifier = Modifier.weight(1f)) {
                 Text(text = "Hello")
-                Text(text = "$name!")
+                Text(
+                    text = "$name!",
+                    style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             OutlinedButton(
                 onClick = {
